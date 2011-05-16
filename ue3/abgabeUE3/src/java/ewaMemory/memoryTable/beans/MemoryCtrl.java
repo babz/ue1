6c4b8f84fb5bfce8a76e046b57ec974f2a50a664
haja <h.jagenteufel@gmail.com>
2011-05-16 22:40:41 +0200

    improved memoryTable.xhtml, but some clicks get lost...

--- ue3/abgabeUE3/src/java/ewaMemory/memoryTable/beans/MemoryCtrl.java
+++ ue3/abgabeUE3/src/java/ewaMemory/memoryTable/beans/MemoryCtrl.java
@@ -1,9 +1,11 @@
 package ewaMemory.memoryTable.beans;
 
+import ewaMemory.memoryTable.api.MemoryAPI;
 import ewaMemory.memoryTable.gui.MemoryTableParams;
 import java.util.Map;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -14,13 +16,37 @@ import sun.util.logging.resources.logging;
 public class MemoryCtrl {
     private static final Logger log = Logger.getLogger(MemoryCtrl.class.getSimpleName());
 
-    public String cardClicked() {
-        Map<String, String> requestMap = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        int x = Integer.parseInt(requestMap.get(MemoryTableParams.X));
-        int y = Integer.parseInt(requestMap.get(MemoryTableParams.Y));
+    @ManagedProperty("#{api}")
+    private MemoryAPI memoryApi;
+    @ManagedProperty("#{memory}")
+    private MemoryTable memoryTable;
+
+    public String cardClicked(int x, int y) {
+//        Map<String, String> requestMap = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+//        int x = Integer.parseInt(requestMap.get(MemoryTableParams.X));
+//        int y = Integer.parseInt(requestMap.get(MemoryTableParams.Y));
 
         log.info("cardClicked at (x,y): (" + x + "," + y + ")");
+        memoryApi.clickOnCard(memoryTable, x, y);
 
         return "memoryTable.xhtml";
+    }
+
+
+
+    public MemoryAPI getMemoryApi() {
+        return memoryApi;
+    }
+
+    public void setMemoryApi(MemoryAPI memoryApi) {
+        this.memoryApi = memoryApi;
+    }
+
+    public MemoryTable getMemoryTable() {
+        return memoryTable;
+    }
+
+    public void setMemoryTable(MemoryTable memoryTable) {
+        this.memoryTable = memoryTable;
     }
 }
