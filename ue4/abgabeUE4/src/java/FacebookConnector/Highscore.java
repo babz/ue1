4f592662085f351f7d283821cb9a2420de878cec
Babz <b08642@gmx.net>
2011-06-13 17:26:43 +0200

    token

--- ue4/abgabeUE4/src/java/FacebookConnector/Highscore.java
+++ ue4/abgabeUE4/src/java/FacebookConnector/Highscore.java
@@ -28,10 +28,17 @@ public class Highscore implements FacebookConnector {
     @Override
     public List<Score> getHighScoreList() throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
+        /*
+         * read Stream:
+         * "https://graph.facebook.com/?ids=https://graph.facebook.com/182396081803634/feed
+         */
     }
 
     @Override
     public Integer publishHighScoreResult(Score score) {
+       /* String url ="https://graph.facebook.com/182396081803634/feed";
+        String msg =url+":curl -F 'access_token="+getAccessToken()
+                +"'\ -F 'message="+score.getFacebookPublicationString()+"'"+url;*/
         return score.getScoreResult();
     }
     
